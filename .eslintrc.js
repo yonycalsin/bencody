074439d8cc25fc22734f0e415782dcda70808aa5
Yony Calsin <helloyonicb@gmail.com>
2020-12-20 15:23:17 -0500

    fix: 🐛 purge (#3)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,53 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    "airbnb",
-    "prettier",
-    "prettier/react",
-    "plugin:react-hooks/recommended",
-  ],
-  plugins: ["react", "prettier", "react-hooks"],
-  rules: { "react/prop-types": 0 },
+   parser: '@typescript-eslint/parser',
+   plugins: ['@typescript-eslint'],
+   extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      // Uncomment the following lines to enable eslint-config-prettier
+      // Is not enabled right now to avoid issues with the Next.js repo
+      'prettier',
+      'prettier/@typescript-eslint',
+   ],
+   env: {
+      es6: true,
+      browser: true,
+      jest: true,
+      node: true,
+   },
+   rules: {
+      'react/react-in-jsx-scope': 0,
+      'react/display-name': 0,
+      'react/prop-types': 0,
+      '@typescript-eslint/explicit-function-return-type': 0,
+      '@typescript-eslint/explicit-member-accessibility': 0,
+      '@typescript-eslint/indent': 0,
+      '@typescript-eslint/member-delimiter-style': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-var-requires': 0,
+      '@typescript-eslint/no-use-before-define': 0,
+      'no-console': [
+         2,
+         {
+            allow: ['warn', 'error'],
+         },
+      ],
+
+      // Customized
+      '@typescript-eslint/triple-slash-reference': 0,
+      '@typescript-eslint/interface-name-prefix': 0,
+      semi: 1,
+      '@typescript-eslint/ban-ts-ignore': 0,
+      /// Override rules if required to follow `airbnb`
+      '@typescript-eslint/no-unused-vars': [
+         'error',
+         {
+            varsIgnorePattern: 'logger',
+            args: 'none',
+            ignoreRestSiblings: true,
+            argsIgnorePattern: '^_',
+         },
+      ],
+   },
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,135 +1,135 @@
-const urljoin = require("url-join");
-const path = require("path");
-const config = require("./data/SiteConfig");
+const urljoin = require('url-join');
+const path = require('path');
+const config = require('./data/SiteConfig');
 
 // Make sure that pathPrefix is not empty
-const validatedPathPrefix = config.pathPrefix === "" ? "/" : config.pathPrefix;
+const validatedPathPrefix = config.pathPrefix === '' ? '/' : config.pathPrefix;
 
 module.exports = {
-  pathPrefix: validatedPathPrefix,
-  siteMetadata: {
-    siteUrl: urljoin(config.siteUrl, config.pathPrefix),
-    rssMetadata: {
-      site_url: urljoin(config.siteUrl, config.pathPrefix),
-      feed_url: urljoin(config.siteUrl, config.pathPrefix, config.siteRss),
-      title: config.siteTitle,
-      description: config.siteDescription,
-      image_url: `${urljoin(
-        config.siteUrl,
-        config.pathPrefix
-      )}/logos/logo-512.png`,
-      copyright: config.copyright,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
+   pathPrefix: validatedPathPrefix,
+   siteMetadata: {
+      siteUrl: urljoin(config.siteUrl, config.pathPrefix),
+      rssMetadata: {
+         site_url: urljoin(config.siteUrl, config.pathPrefix),
+         feed_url: urljoin(config.siteUrl, config.pathPrefix, config.siteRss),
+         title: config.siteTitle,
+         description: config.siteDescription,
+         image_url: `${urljoin(
+            config.siteUrl,
+            config.pathPrefix,
+         )}/logos/logo-512.png`,
+         copyright: config.copyright,
       },
-    },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-lodash",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "assets",
-        path: `${__dirname}/static/`,
+   },
+   plugins: [
+      {
+         resolve: `gatsby-plugin-typescript`,
+         options: {
+            isTSX: true, // defaults to false
+            jsxPragma: `jsx`, // defaults to "React"
+            allExtensions: true, // defaults to false
+         },
       },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "posts",
-        path: `${__dirname}/content/`,
+      'gatsby-plugin-react-helmet',
+      'gatsby-plugin-lodash',
+      {
+         resolve: 'gatsby-source-filesystem',
+         options: {
+            name: 'assets',
+            path: `${__dirname}/static/`,
+         },
       },
-    },
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-relative-images`,
-          },
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 690,
+      {
+         resolve: 'gatsby-source-filesystem',
+         options: {
+            name: 'posts',
+            path: `${__dirname}/content/`,
+         },
+      },
+      {
+         resolve: 'gatsby-transformer-remark',
+         options: {
+            plugins: [
+               {
+                  resolve: `gatsby-remark-relative-images`,
+               },
+               {
+                  resolve: 'gatsby-remark-images',
+                  options: {
+                     maxWidth: 690,
+                  },
+               },
+               {
+                  resolve: 'gatsby-remark-responsive-iframe',
+               },
+               'gatsby-remark-copy-linked-files',
+               'gatsby-remark-autolink-headers',
+               'gatsby-remark-prismjs',
+            ],
+         },
+      },
+      {
+         resolve: 'gatsby-plugin-google-analytics',
+         options: {
+            trackingId: config.googleAnalyticsID,
+         },
+      },
+      {
+         resolve: 'gatsby-plugin-nprogress',
+         options: {
+            color: config.themeColor,
+         },
+      },
+      'gatsby-plugin-sharp',
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-catch-links',
+      'gatsby-plugin-twitter',
+      'gatsby-plugin-sitemap',
+      {
+         resolve: 'gatsby-plugin-manifest',
+         options: {
+            name: config.siteTitle,
+            short_name: config.siteTitleShort,
+            description: config.siteDescription,
+            start_url: validatedPathPrefix,
+            background_color: config.backgroundColor,
+            theme_color: config.themeColor,
+            display: 'minimal-ui',
+            icons: [
+               {
+                  src: '/logos/logo-192.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+               },
+               {
+                  src: '/logos/logo-512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+               },
+            ],
+         },
+      },
+      'gatsby-plugin-offline',
+      {
+         resolve: 'gatsby-plugin-netlify-cms',
+         options: {
+            modulePath: path.resolve('src/netlifycms/index.js'), // default: undefined
+            enableIdentityWidget: true,
+            publicPath: 'admin',
+            htmlTitle: 'Content Manager',
+            includeRobots: false,
+         },
+      },
+      {
+         resolve: 'gatsby-plugin-feed',
+         options: {
+            setup(ref) {
+               const ret = ref.query.site.siteMetadata.rssMetadata;
+               ret.allMarkdownRemark = ref.query.allMarkdownRemark;
+               ret.generator = 'GatsbyJS Advanced Starter';
+               return ret;
             },
-          },
-          {
-            resolve: "gatsby-remark-responsive-iframe",
-          },
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers",
-          "gatsby-remark-prismjs",
-        ],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: config.googleAnalyticsID,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-nprogress",
-      options: {
-        color: config.themeColor,
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-catch-links",
-    "gatsby-plugin-twitter",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: config.siteTitle,
-        short_name: config.siteTitleShort,
-        description: config.siteDescription,
-        start_url: validatedPathPrefix,
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: "minimal-ui",
-        icons: [
-          {
-            src: "/logos/logo-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/logos/logo-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    },
-    "gatsby-plugin-offline",
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: path.resolve("src/netlifycms/index.js"), // default: undefined
-        enableIdentityWidget: true,
-        publicPath: "admin",
-        htmlTitle: "Content Manager",
-        includeRobots: false,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-feed",
-      options: {
-        setup(ref) {
-          const ret = ref.query.site.siteMetadata.rssMetadata;
-          ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-          ret.generator = "GatsbyJS Advanced Starter";
-          return ret;
-        },
-        query: `
+            query: `
         {
           site {
             siteMetadata {
@@ -145,24 +145,24 @@ module.exports = {
           }
         }
       `,
-        feeds: [
-          {
-            serialize(ctx) {
-              const { rssMetadata } = ctx.query.site.siteMetadata;
-              return ctx.query.allMarkdownRemark.edges.map((edge) => ({
-                categories: edge.node.frontmatter.tags,
-                date: edge.node.fields.date,
-                title: edge.node.frontmatter.title,
-                description: edge.node.excerpt,
-                url: rssMetadata.site_url + edge.node.fields.slug,
-                guid: rssMetadata.site_url + edge.node.fields.slug,
-                custom_elements: [
-                  { "content:encoded": edge.node.html },
-                  { author: config.userEmail },
-                ],
-              }));
-            },
-            query: `
+            feeds: [
+               {
+                  serialize(ctx) {
+                     const { rssMetadata } = ctx.query.site.siteMetadata;
+                     return ctx.query.allMarkdownRemark.edges.map((edge) => ({
+                        categories: edge.node.frontmatter.tags,
+                        date: edge.node.fields.date,
+                        title: edge.node.frontmatter.title,
+                        description: edge.node.excerpt,
+                        url: rssMetadata.site_url + edge.node.fields.slug,
+                        guid: rssMetadata.site_url + edge.node.fields.slug,
+                        custom_elements: [
+                           { 'content:encoded': edge.node.html },
+                           { author: config.userEmail },
+                        ],
+                     }));
+                  },
+                  query: `
             {
               allMarkdownRemark(
                 limit: 1000,
@@ -189,11 +189,11 @@ module.exports = {
               }
             }
           `,
-            output: config.siteRss,
-            title: config.siteRssTitle,
-          },
-        ],
+                  output: config.siteRss,
+                  title: config.siteRssTitle,
+               },
+            ],
+         },
       },
-    },
-  ],
+   ],
 };
