c35aed1c78e6346f3e86cd8e8dee9c052c955525
Yony Calsin <helloyonicb@gmail.com>
2020-12-20 13:16:02 -0500

    feat: typescript setup (#2)

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,11 +41,11 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const postPage = path.resolve("src/templates/post.jsx");
-  const tagPage = path.resolve("src/templates/tag.jsx");
-  const categoryPage = path.resolve("src/templates/category.jsx");
-  const listingPage = path.resolve("./src/templates/listing.jsx");
-  const landingPage = path.resolve("./src/templates/landing.jsx");
+  const postPage = path.resolve("src/templates/post.tsx");
+  const tagPage = path.resolve("src/templates/tag.tsx");
+  const categoryPage = path.resolve("src/templates/category.tsx");
+  const listingPage = path.resolve("./src/templates/listing.tsx");
+  const landingPage = path.resolve("./src/templates/landing.tsx");
 
   // Get a full list of markdown posts
   const markdownQueryResult = await graphql(`
