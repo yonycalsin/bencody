f19832ebe0a4d0b6b7fca66639c9d201f9fc1cc7
Yony Calsin <helloyonicb@gmail.com>
2020-12-26 17:39:15 -0500

    feat: 🎸 add typography component

---
+++ src/utils/get-text-style.ts
@@ -0,0 +1,7 @@
+const getTextStyle = (style) => `
+   font-size: ${style.size}px;
+   font-weight: ${style.weight};
+   line-height: ${style.lineHeight}px;
+`;
+
+export default getTextStyle;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as config } from './config';
 export { default as getSizeStyle } from './get-size-style';
+export { default as getTextStyle } from './get-text-style';
 export { default as theme } from './theme';
